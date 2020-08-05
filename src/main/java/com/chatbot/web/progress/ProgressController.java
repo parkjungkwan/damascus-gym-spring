7a622bb231e486e8cbf18260c6a032515d682e77
parkjungkwan <pakjkwan@gmail.com>
2020-08-05 21:37:01 +0900

    21:36

--- src/main/java/com/chatbot/web/progress/ProgressController.java
+++ src/main/java/com/chatbot/web/progress/ProgressController.java
@@ -1,4 +1,13 @@
 package com.chatbot.web.progress;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProgressController {
+    @ResponseBody @GetMapping("/hello")
+    public String process(){
+        return "Hello Controller";
+    }
 }
