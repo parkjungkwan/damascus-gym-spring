4b4e264bf0f74c23180e5faa0d0ec7dd90feae51
parkjungkwan <pakjkwan@gmail.com>
2020-08-07 10:06:08 +0900

    9:04

--- src/main/java/com/chatbot/web/passenger/Passenger.java
+++ src/main/java/com/chatbot/web/passenger/Passenger.java
@@ -1,5 +1,31 @@
 package com.chatbot.web.passenger;
+/*
+Variable Notes
+// 0-PassengerId, 1-Survived,2-Pclass,3-Name,4-Gender,
+// 5-Age,6-SibSp, 7-Parch, 8-Ticket, 9-Fare,
+// 10-Cabin, 11-Embarked
+1-PassengerId: Primary Key
+2-pclass: Ticket class
+A proxy for socio-economic status (SES)
+1st = Upper
+2nd = Middle
+3rd = Lower
+5-age: Age is fractional if less than 1. If the age is estimated, is it in the form of xx.5
+6-sibsp: # of siblings / spouses aboard the Titanic
+The dataset defines family relations in this way...
+Sibling = brother, sister, stepbrother, stepsister
+Spouse = husband, wife (mistresses and fiancés were ignored)
 
+7-parch: # of parents / children aboard the Titanic
+The dataset defines family relations in this way...
+Parent = mother, father
+Child = daughter, son, stepdaughter, stepson
+Some children travelled only with a nanny, therefore parch=0 for them.
+8-Ticket: Ticket number
+9-Fare: Passenger fare
+10-Cabin: Cabin number
+11-Embarked: Port of Embarkation,  C = Cherbourg, Q = Queenstown, S = Southampton
+* */
 import lombok.*;
 
 import javax.persistence.*;
